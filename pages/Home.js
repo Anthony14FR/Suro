7910543bf229e9dfd51486ec03439fa644c4428e
AZARKANE Issam <issam.azarkane@gmail.com>
2024-05-31 03:14:47 +0200

    UP: Home Banner front

--- pages/Home.js
+++ pages/Home.js
@@ -3,21 +3,22 @@ import Navbar from "../components/Navbar.js";
 
 export default function Home() {
   const div = document.createElement("div");
+  div.className = "py-5";
   div.appendChild(Navbar());
 
-  const mainContent = document.createElement("div");
-  mainContent.className = "p-5";
+  // const mainContent = document.createElement("div");
+  // mainContent.className = "p-5";
 
-  const heading = document.createElement("h1");
-  heading.className = "text-3xl font-bold";
-  heading.textContent = "Welcome to the Home Page";
-  mainContent.appendChild(heading);
+  // const heading = document.createElement("h1");
+  // heading.className = "text-3xl font-bold";
+  // heading.textContent = "Welcome to the Home Page";
+  // mainContent.appendChild(heading);
 
-  const paragraph = document.createElement("p");
-  paragraph.className = "mt-4 text-lg";
-  paragraph.textContent = "This is the home page of our simple application.";
-  mainContent.appendChild(paragraph);
+  // const paragraph = document.createElement("p");
+  // paragraph.className = "mt-4 text-lg";
+  // paragraph.textContent = "This is the home page of our simple application.";
+  // mainContent.appendChild(paragraph);
 
-  div.appendChild(mainContent);
+  // div.appendChild(mainContent);
   return div;
 }
